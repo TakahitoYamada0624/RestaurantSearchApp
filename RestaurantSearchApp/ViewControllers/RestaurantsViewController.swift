252e88565f9a79c702196071c1f42500748ecd56
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-14 23:28:12 +0900

    first commit

--- RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
+++ RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RestaurantsViewController.swift
 //  RestaurantSearchApp
 //
 //  Created by Takahito Yamada on 2021/05/13.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RestaurantsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
-
-
 }
-
---
+++ RestaurantSearchApp/Views/RestaurantCell.swift
@@ -0,0 +1,12 @@
+//
+//  RestaurantCell.swift
+//  RestaurantSearchApp
+//
+//  Created by Takahito Yamada on 2021/05/14.
+//
+
+import UIKit
+
+class RestaurantCell: UITableViewCell {
+    
+}
