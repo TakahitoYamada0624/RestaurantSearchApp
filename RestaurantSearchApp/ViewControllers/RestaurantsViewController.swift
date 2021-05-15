9aceca76a05f66cb140cc22e5aa7cae2e616dd3a
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-15 16:33:10 +0900

    TableViewの実装

--- RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
+++ RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class RestaurantsViewController: UIViewController {
-
+    
+    @IBOutlet weak var restaurantsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("")
+        
+        restaurantsTableView.dataSource = self
+        restaurantsTableView.delegate = self
+        restaurantsTableView.register(UINib(nibName: "RestaurantCell", bundle: nil), forCellReuseIdentifier: "RestaurantCell")
+        
     }
+}
+
+extension RestaurantsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        return cell
+    }
+    
 }
