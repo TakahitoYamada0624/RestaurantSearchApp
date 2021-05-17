c8304225c70a372e951088f9ef46869b90074a14
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-17 22:59:51 +0900

    位置情報取得の処理を削除

--- RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
+++ RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
@@ -6,22 +6,13 @@
 //
 
 import UIKit
-import CoreLocation
 
 class RestaurantsViewController: UIViewController {
     
     @IBOutlet weak var restaurantsTableView: UITableView!
     
-    var locationManager: CLLocationManager!
-    
-    private var latitude: CLLocationDegrees = 0
-    private var longitude: CLLocationDegrees = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
         
         restaurantsTableView.dataSource = self
         restaurantsTableView.delegate = self
@@ -43,51 +34,6 @@ extension RestaurantsViewController: UITableViewDataSource, UITableViewDelegate 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
         return cell
-    }
-    
-}
-
-extension RestaurantsViewController: CLLocationManagerDelegate {
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        switch manager.authorizationStatus {
-        case .authorizedAlways:
-            print("authorizedAlways")
-            
-        case .authorizedWhenInUse:
-            print("authorizedWhenInUse")
-            locationManager.startUpdatingLocation()
-            
-        case .denied, .restricted:
-            print("denied, restricted")
-            showAlert()
-            
-        case .notDetermined:
-            print("notDetermined")
-            manager.requestWhenInUseAuthorization()
-            
-        default:
-        print("扱っていないケースが発生しました。")
-        }
-        
-    }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "位置情報の確認", message:  "レストランを検索しやすくするために設定から位置情報を許可してください" , preferredStyle: .alert)
-        let ok = UIAlertAction(title: "閉じる", style: .default) { (action) in
-            self.dismiss(animated: true, completion: nil)
-        }
-        alert.addAction(ok)
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.first
-        let latitude = location?.coordinate.latitude
-        let longitude = location?.coordinate.longitude
-        self.latitude = latitude ?? 0
-        self.longitude = longitude ?? 0
-        print("latitude:", self.latitude)
-        print("longitude:", self.longitude)
     }
     
 }
