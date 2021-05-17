4a549c8eb4c0126be24f3560591f24da140543e1
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-17 17:48:12 +0900

    検索画面の追加

---
+++ RestaurantSearchApp/ViewControllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  RestaurantSearchApp
+//
+//  Created by Takahito Yamada on 2021/05/17.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
