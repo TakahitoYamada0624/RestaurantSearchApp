4e2d94b902cec8310edb27251637310c18d51346
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-21 01:39:48 +0900

    APIからの取得と表示の処理を実装

--- RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
+++ RestaurantSearchApp/ViewControllers/RestaurantsViewController.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import Alamofire
 
 class RestaurantsViewController: UIViewController {
     
     @IBOutlet weak var restaurantsTableView: UITableView!
+    
+    var restaurants = [Restaurant]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +20,41 @@ class RestaurantsViewController: UIViewController {
         restaurantsTableView.dataSource = self
         restaurantsTableView.delegate = self
         restaurantsTableView.register(UINib(nibName: "RestaurantCell", bundle: nil), forCellReuseIdentifier: "RestaurantCell")
+        getRestarantsInfo()
+    }
+    
+    func getRestarantsInfo() {
+        let url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?"
+        let apiKey = "c7355e2429b8ed1a"
+        let parameters = [
+            "key": apiKey,
+            "lat": 35.680959106959,
+            "lng": 139.76730676352,
+            "range": 3,
+            "count": 50,
+            "format": "json"
+        ] as [String : Any]
         
+        AF.request(url, method: .get, parameters: parameters).responseJSON { (response) in
+            do {
+                guard let data = response.data else {return}
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(Result.self, from: data)
+                print("result", result.results.restaurants[1].name)
+                self.restaurants = result.results.restaurants
+                print("restaurants:", self.restaurants.count)
+                self.restaurantsTableView.reloadData()
+            }catch{
+                print("失敗しました", error)
+            }
+        }
     }
     
 }
 
 extension RestaurantsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 15
+        return restaurants.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -32,7 +62,11 @@ extension RestaurantsViewController: UITableViewDataSource, UITableViewDelegate 
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "RestaurantCell", for: indexPath) as! RestaurantCell
+        let restaurant = restaurants[indexPath.row]
+        cell.name = restaurant.name
+        cell.access = restaurant.access
+        cell.thumnailString = restaurant.photo.mobile.thumbnail
         return cell
     }
     
