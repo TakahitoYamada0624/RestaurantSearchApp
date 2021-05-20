20968e7ce8077a26684d343e588eba962c5191ba
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-21 01:38:30 +0900

    Restaurantクラスを作成

--- RestaurantSearchApp/Models/Restaurant.swift
+++ RestaurantSearchApp/Models/Restaurant.swift
@@ -14,8 +14,28 @@ class Result: Decodable {
 
 class Restaurants: Decodable {
     let matchCount: Int
+    let restaurants: [Restaurant]
     
     enum CodingKeys: String, CodingKey {
         case matchCount = "results_available"
+        case restaurants = "shop"
+    }
+}
+
+class Restaurant: Decodable {
+    let name: String
+    let access: String
+    let photo: Photo
+}
+
+class Photo: Decodable {
+    let mobile: Thumbnail
+}
+
+class Thumbnail: Decodable {
+    let thumbnail: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case thumbnail = "l"
     }
 }
