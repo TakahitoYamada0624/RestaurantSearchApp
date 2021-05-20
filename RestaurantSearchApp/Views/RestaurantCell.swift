b5aff5ccd90520a69b2c5b2ddc7c1ee10811345a
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-21 01:41:32 +0900

    画像とラベルに情報を表示

--- RestaurantSearchApp/Views/RestaurantCell.swift
+++ RestaurantSearchApp/Views/RestaurantCell.swift
@@ -14,4 +14,29 @@ class RestaurantCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var accessLabel: UILabel!
     
+    var name: String? {
+        didSet {
+            nameLabel.text = name
+        }
+    }
+    
+    var access: String? {
+        didSet {
+            accessLabel.text = access
+        }
+    }
+    
+    var thumnailString: String? {
+        didSet {
+            guard let url = URL(string: thumnailString ?? "") else {return}
+            do {
+                let data = try Data(contentsOf: url)
+                thumbnailImageView.image = UIImage(data: data)
+                thumbnailImageView.contentMode = .scaleAspectFill
+            }catch{
+                print("データの変換に失敗しました。")
+            }
+        }
+    }
+    
 }
