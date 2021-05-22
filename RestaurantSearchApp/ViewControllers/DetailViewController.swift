e2111610efb95983711f28b811ad75cde0922e5d
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-22 14:18:27 +0900

    レストランの詳細情報をAPIから取得

--- RestaurantSearchApp/ViewControllers/DetailViewController.swift
+++ RestaurantSearchApp/ViewControllers/DetailViewController.swift
@@ -6,24 +6,39 @@
 //
 
 import UIKit
+import Alamofire
 
 class DetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var basicInformationView: BasicInformationView!
+    
+    private let url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?"
+    private let apiKey = "c7355e2429b8ed1a"
+    
+    var id: String = "J001251597"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        getRestaurantDetailInfo()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func getRestaurantDetailInfo() {
+        let parameter = [
+            "key": apiKey,
+            "id": id,
+            "format": "json"
+        ]
+        
+        AF.request(url, method: .get, parameters: parameter).responseJSON { (response) in
+            do {
+                guard let data = response.data else {return}
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(DetailResult.self, from: data)
+                print("result", result.results.restaurants.first?.address)
+                self.basicInformationView.name = result.results.restaurants[0].name
+            }catch{
+                print("エラーが発生しました。", error)
+            }
+        }
     }
-    */
-
 }
