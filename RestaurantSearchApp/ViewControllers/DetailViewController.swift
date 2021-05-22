f79404d903eb7e729a7ededda8a2a27f9558874a
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-22 20:34:22 +0900

    カスタムビューにAPIからの情報を渡す処理を実装

--- RestaurantSearchApp/ViewControllers/DetailViewController.swift
+++ RestaurantSearchApp/ViewControllers/DetailViewController.swift
@@ -13,11 +13,11 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var basicInformationView: BasicInformationView!
     @IBOutlet weak var paymentView: PaymentView!
     @IBOutlet weak var foodDrinkView: FoodDrinkView!
+    @IBOutlet weak var restaurantImageView: UIImageView!
     
     private let url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?"
     private let apiKey = "c7355e2429b8ed1a"
-    
-    var id: String = "J001251597"
+    var id: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -56,5 +56,14 @@ class DetailViewController: UIViewController {
         foodDrinkView.course = restaurant.course
         foodDrinkView.freeDrink = restaurant.freeDrink
         foodDrinkView.freeFood = restaurant.freeFood
+        let str = restaurant.photo.mobile.picture
+        guard let url = URL(string: str) else {return}
+        do {
+            let data = try Data(contentsOf: url)
+            restaurantImageView.image = UIImage(data: data)
+            restaurantImageView.contentMode = .scaleAspectFill
+        }catch{
+            print("失敗しました。")
+        }
     }
 }
