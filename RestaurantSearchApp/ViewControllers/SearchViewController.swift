a6d4577ebeb3900edff662f1e02542c2f8136451
TakahitoYamada0624 <antt.pacific.jp@gmail.com>
2021-05-23 17:34:52 +0900

    検索窓から検索ができるようにした

--- RestaurantSearchApp/ViewControllers/SearchViewController.swift
+++ RestaurantSearchApp/ViewControllers/SearchViewController.swift
@@ -13,46 +13,57 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var distanceSlider: UISlider!
     @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     var locationManager: CLLocationManager!
     
     private var latitude: CLLocationDegrees = 0
     private var longitude: CLLocationDegrees = 0
-    var parameters = [String: Any]()
+    var addParameters = [String: Any]()
     let apiRequest = APIRequest()
+    private var notChooseDistance: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         locationManager = CLLocationManager()
         locationManager.delegate = self
+        searchBar.delegate = self
         
+        getRestaurantsCount()
     }
     
     //スライダーを動かしている時に呼ばれる
     @IBAction func selectDistance(_ sender: Any) {
+        
         let value = distanceSlider.value
         
         switch value {
         case 0 ..< 0.2:
+            distanceLabel.text = "300m"
             print("300m")
         case 0.2 ..< 0.4:
+            distanceLabel.text = "500m"
             print("500m")
         case 0.4 ..< 0.6:
+            distanceLabel.text = "1,000m"
             print("1,000m")
         case 0.6 ..< 0.8:
+            distanceLabel.text = "2,000m"
             print("2,000m")
         case 0.8 ... 1:
+            distanceLabel.text = "3,000m"
             print("3,000m")
         default:
             print("予想外の挙動が起きました")
         }
-        print("value: ", value)
     }
     
     //スライダーから指が離れた時に呼ばれる
     @IBAction func releasedSlider(_ sender: Any) {
         print("スライダーを外しました")
+        notChooseDistance = false
         getRestaurantsCount()
     }
     
@@ -61,16 +72,15 @@ class SearchViewController: UIViewController {
         print("検索ボタンが押されました。")
         let storyboard = UIStoryboard(name: "Restaurants", bundle: nil)
         let restaurants = storyboard.instantiateViewController(withIdentifier: "Restaurants") as! RestaurantsViewController
-        restaurants.parameters = self.parameters
+        restaurants.parameters = self.addParameters
         navigationController?.pushViewController(restaurants, animated: true)
     }
     
     //条件に合うレストラン数を取得する
     func getRestaurantsCount() {
-        
         var range: Int = 0
         let value = distanceSlider.value
-
+        
         switch value {
         case 0 ..< 0.2:
             range = 1
@@ -86,10 +96,48 @@ class SearchViewController: UIViewController {
             print("予想外の挙動が起きました")
         }
         
-        apiRequest.getRestaurantsInfo(lat: self.latitude, lng: self.longitude, range: range, start: 1) { (restaurants) in
-            self.searchButton.setTitle("\(restaurants.matchCount)件", for: .normal)
-            print("restaurantName", restaurants.matchCount)
+        let searchTextIsEmpty = searchBar.text?.isEmpty ?? false
+        let notSelectedDistance = notChooseDistance
+        var addPara = [String: Any]()
+        if searchTextIsEmpty && notSelectedDistance {
+            addPara = [
+                "start": 1,
+                "large_area": "Z011"
+            ]
+        }else if searchTextIsEmpty {
+            addPara = [
+                "lat": self.latitude,
+                "lng": self.longitude,
+                "range": range,
+                "start": 1
+            ]
+        }else if notSelectedDistance {
+            guard let searchText = searchBar.text else {return}
+            addPara = [
+                "start": 1,
+                "name_any": searchText
+            ]
+        }else{
+            guard let searchText = searchBar.text else {return}
+            addPara = [
+                "lat": self.latitude,
+                "lng": self.longitude,
+                "range": range,
+                "name_any": searchText,
+                "start": 1
+            ]
         }
+        
+        apiRequest.getRestaurantsInfo(addParameters: addPara){ (restaurants) in
+            self.searchButton.setTitle("\(restaurants.matchCount)件", for: .normal)
+            print("restaurantCount", restaurants.matchCount)
+            self.addParameters = addPara
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        getRestaurantsCount()
     }
 }
 
@@ -98,19 +146,25 @@ extension SearchViewController: CLLocationManagerDelegate {
         switch manager.authorizationStatus {
         case .authorizedAlways:
             print("authorizedAlways")
-            
+            distanceSlider.isEnabled = true
+
         case .authorizedWhenInUse:
             print("authorizedWhenInUse")
             locationManager.startUpdatingLocation()
-            
+            distanceSlider.isEnabled = true
+
         case .denied, .restricted:
             print("denied, restricted")
             showAlert()
-            
+            distanceSlider.isEnabled = false
+            notChooseDistance = true
+
         case .notDetermined:
             print("notDetermined")
             manager.requestWhenInUseAuthorization()
-            
+            distanceSlider.isEnabled = false
+            notChooseDistance = true
+
         default:
             print("扱っていないケースが発生しました。")
         }
@@ -134,5 +188,11 @@ extension SearchViewController: CLLocationManagerDelegate {
         print("latitude:", self.latitude)
         print("longitude:", self.longitude)
     }
-    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        getRestaurantsCount()
+    }
 }
